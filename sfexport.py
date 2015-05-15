be34f3dc8bc98561514cd1fb79c38bd3a91a0317
Matt Hayward <matt@viblio.com>
2015-05-14 17:25:14 -0700

    Bug fixes

--- sfexport.py
+++ sfexport.py
@@ -228,7 +228,7 @@ def sync_salesforce( sponsor_event_id=default_sponsor_event_id, sponsors=None ):
                     discount_code['badge_type_name'] = badge_types[discount_code['badge_type']]['name']
                     discount_code['regonline_url'] = badge_types[discount_code['badge_type']]['regonline_url']
                     
-                    discount_code['discount_search_url'] = "%s%s?code=%s" % ( app.config['EXTERNAL_SERVER_BASE_URL'], 'discount_code/', discount_code['discount_code'] )
+                    discount_code['discount_search_url'] = "%s/%s?code=%s" % ( app.config['EXTERNAL_SERVER_BASE_URL'], 'discount_code/', discount_code['discount_code'] )
                     mail_codes.append( discount_code )
 
                 if len( mail_codes ) > 0:
